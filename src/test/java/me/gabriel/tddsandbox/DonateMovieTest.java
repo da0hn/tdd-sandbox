5b757ef1548435bb4a700fa90d77b354650ce0d4
Gabriel Honda <gabrielhondacba@gmail.com>
2021-10-11 09:02:11 -0400

    test: garante funcionamento das entidades `Library` e `Movie`

---
+++ src/test/java/me/gabriel/tddsandbox/DonateMovieTest.java
@@ -0,0 +1,34 @@
+package me.gabriel.tddsandbox;
+
+import me.gabriel.tddsandbox.donatemovie.Library;
+import me.gabriel.tddsandbox.donatemovie.Movie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DonateMovieTest {
+
+  private Movie movie;
+  private Library library;
+
+  @BeforeEach
+  void setUp() {
+    this.library = new Library();
+    this.movie = new Movie();
+  }
+
+  @Test
+  void movieAddedToCatalogue() {
+    this.library.donate(this.movie);
+    assertTrue(this.library.contains(this.movie));
+  }
+
+  @Test
+  void rentalCopyAdded() {
+    this.library.donate(this.movie);
+    assertEquals(1, this.movie.getCopies());
+  }
+
+}
